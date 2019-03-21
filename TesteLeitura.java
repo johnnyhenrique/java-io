6d96294f650155fc4004311b59bc2389108eb3de
Jonatan Henrique <henrique.oliveirajonatan@gmail.com>
2019-03-21 19:38:53 -0300

    Alteração para teste

---
+++ TesteLeitura.java
@@ -0,0 +1,41 @@
+
+/**
+ * Stream e Reader
+ * @author Jonatan
+ * Stream lê bits e bytes
+ * Reader char
+ * Class InputStreamReader - Transforma bytes em char / Extends InputStream
+ * Class BufferedReader - Representa o conjunto de char / Extends Reader
+ * Classes mais genéricas: vide TesteLeitura2
+ */
+
+package br.com.alura.teste;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class TesteLeitura {
+
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream fis = new FileInputStream("lorem.txt"); // entrada
+		InputStreamReader isr = new InputStreamReader(fis); // bits em char lista
+		BufferedReader br = new BufferedReader(isr); // buffer para ler texto
+		
+		String linha = br.readLine(); //buffer tem metodo para ler linha
+		
+		while(linha != null) { // enquanto linha não nula sem conteudo
+			System.out.println(linha); // le linha
+			linha = br.readLine(); // linha atualiza
+		}
+		
+		
+		
+		br.close(); // fecha entrada
+		
+	}
+
+}
